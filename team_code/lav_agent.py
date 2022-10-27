af521897aed7a39fbc1c35c188a4a754aba28730
Anthony Hu <anth.hu@gmail.com>
2022-10-27 15:33:20 +0100

    remove vis

--- team_code/lav_agent.py
+++ team_code/lav_agent.py
@@ -178,11 +178,6 @@ class LAVAgent(AutonomousAgent):
         rgb = np.concatenate(rgbs, axis=1)
         all_rgb = np.stack(rgbs, axis=0)
 
-        vis_path = '/home/anthony/experiments/lav_carla/vis'
-        os.makedirs(vis_path, exist_ok=True)
-        from PIL import Image
-        Image.fromarray(np.hstack(rgbs)).save(os.path.join(vis_path, f'{self.num_frames:06d}.png'))
-
         if self.waypointer is None:
 
             self.waypointer = Waypointer(
