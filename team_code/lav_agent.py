de0d648aa314776919d9274ca424343915e52d2b
Anthony Hu <anth.hu@gmail.com>
2022-10-27 15:23:35 +0100

    fix vis

--- team_code/lav_agent.py
+++ team_code/lav_agent.py
@@ -181,7 +181,7 @@ class LAVAgent(AutonomousAgent):
         vis_path = '/home/anthony/experiments/lav_carla/vis'
         os.makedirs(vis_path, exist_ok=True)
         from PIL import Image
-        Image.fromarray(all_rgb).save(os.path.join(vis_path, f'{self.num_frames:06d}.png'))
+        Image.fromarray(np.hstack(rgbs)).save(os.path.join(vis_path, f'{self.num_frames:06d}.png'))
 
         if self.waypointer is None:
 
